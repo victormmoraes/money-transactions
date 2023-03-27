1bab4e2a917bf84920192145ea325d47b99a66ac
Victor Medrado <victormedrado49@gmail.com>
2023-03-27 17:24:47 -0300

    Criando radio button acessível com radix-ui e estilizando botoes

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,6 +1,6 @@
 import * as Dialog from '@radix-ui/react-dialog';
-import { X } from 'phosphor-react';
-import { CloseButton, Content, Overlay } from './styles';
+import { ArrowCircleDown, ArrowCircleUp, X } from 'phosphor-react';
+import { CloseButton, Content, Overlay, TransactionType, TransactionTypeButton } from './styles';
 
 export function NewTransactionModal() {
   return (
@@ -10,7 +10,7 @@ export function NewTransactionModal() {
       <Content>
         <Dialog.Title>Nova transação</Dialog.Title>
         <CloseButton>
-          <X  size={24}/>
+          <X size={24} />
         </CloseButton>
 
         <form action="">
@@ -18,12 +18,24 @@ export function NewTransactionModal() {
           <input type="number" placeholder='Preço' required />
           <input type="text" placeholder='Categoria' required />
 
+          <TransactionType>
+            <TransactionTypeButton variant='income' value='income'>
+              <ArrowCircleUp size={24} />
+              Entrada
+            </TransactionTypeButton>
+
+            <TransactionTypeButton variant='outcome' value='outcome'>
+              <ArrowCircleDown size={24} />
+              Saída
+            </TransactionTypeButton>
+          </TransactionType>
+
           <button type="submit">
             Cadastrar
           </button>
         </form>
 
-        
+
       </Content>
     </Dialog.Portal>
   )
