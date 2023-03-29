bdfb3df0d3b257df369f5c528583020435754c99
Victor Medrado <victormedrado49@gmail.com>
2023-03-29 19:38:29 -0300

    Aplicando useMemo

--- src/contexts/TransactionsContext.tsx
+++ src/contexts/TransactionsContext.tsx
@@ -60,7 +60,7 @@ export function TransactionsProvider({ children }: TransactionsProviderProps) {
 
   useEffect(() => {
     fetchTransactions()
-  }, [])
+  }, [fetchTransactions])
 
   return (
     <TransactionsContext.Provider
